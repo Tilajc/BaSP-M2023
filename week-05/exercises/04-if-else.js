4d2966a89f2d8816bcc430d35203f12fad76357d
Juan Cruz Tilatti <jctilatti@gmail.com>
2023-04-14 18:52:28 -0300

    BaSP-W05-2023: Finished for section, added title in previus section

--- week-05/exercises/04-if-else.js
+++ week-05/exercises/04-if-else.js
@@ -1,5 +1,6 @@
 //04. If-Else
 
+console.log('04. If-Else');
 
 /*a. Crear un número aleatorio entre 0 y 1 utilizando la función Math.random(), si el valor es mayor o igual que 0,5
 mostrar una alerta con el mensaje “Greater than or equal to 0,5” y sino un alerta con el mensaje “Lower than 0,5”.*/
