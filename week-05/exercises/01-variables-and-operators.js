42ca5ce42ed39dfc4f8f06f4b3322f83bfde86c4
Juan Cruz Tilatti <jctilatti@gmail.com>
2023-04-14 14:43:02 -0300

    BaSP-W05-2023: Finished 02-strings section

--- week-05/exercises/01-variables-and-operators.js
+++ week-05/exercises/01-variables-and-operators.js
@@ -1,11 +1,11 @@
 //01. Variables y Operadores
 
-console.log('01.Variables y Operadores');
+console.log('01.Variables and Operators');
 
 /* a. Crear dos variables numéricas y utilizar el operador suma
 para guardar el valor de la suma de ambos números en una 3er variable.*/
 
-console.log('01.a.');
+console.log('01.a:');
 
 var n1= 10;
 var n2= 20;
@@ -15,7 +15,7 @@ console.log(n1, '+', n2, '=', addition);
 
 //b. Crear dos variables de tipo String y concatenarlas guardando el resultado en una 3er variable.
 
-console.log('01.b.');
+console.log('01.b:');
 
 var str1 = 'hello';
 var str2 = ' world';
@@ -26,7 +26,7 @@ console.log(str1 + str2, '=', str3);
 /*c. Crear dos variables de tipo String y sumar el largo de cada variable (cantidad de letras del string)
 guardando el resultado de la suma en una 3er variable (utilizar length)*/
 
-console.log('01.c.');
+console.log('01.c:');
 
 var str4 = 'first string';
 var str5 = 'second string';
--- week-05/exercises/02-strings.js
+++ week-05/exercises/02-strings.js
@@ -1,23 +1,66 @@
 //02. Strings
 
+console.log('02. Strings');
 
 /*a. Crear una variable de tipo string con al menos 10 caracteres y
 convertir todo el texto en mayúscula (utilizar toUpperCase).*/
 
+console.log('02.a:');
+
+var str1 = 'string de al menos 10 caracteres';
+
+console.log(str1.toUpperCase());
+
 /*b. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string
 con los primeros 5 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
+
+console.log('02.b:');
+
+var str2 = 'string dos de al menos 10 caracteres';
+
+console.log('Las primeras cinco letras del string son: ', str2.substring(0,5));
 
 /*c. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string
 con los últimos 3 caracteres guardando el resultado en una nueva variable (utilizar substring).*/
 
-/*d. Crear una variable de tipo string con al menos 10 caracteres y generar un nuevo string
- con la primera letra en mayúscula y las demás en minúscula.
- uardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).*/
+console.log('02.c:');
+
+var str3 = 'string tres de al menos 10 caracteres';
+
+console.log('las ultimas tres letras del string son: ', str3.substring((str3.length-3),str3.length));
+
+/*d. Crear una variable de tipo string con al menos 10 caracteres y
+generar un nuevo string con la primera letra en mayúscula y las demás en minúscula.
+Guardar el resultado en una nueva variable (utilizar substring, toUpperCase, toLowerCase y el operador +).*/
+
+console.log('02.d:');
+
+var str4 = 'string cuatro de al menos 10 caractereS';
+var dResult = str4.substring(0,1).toUpperCase() + str4.substring(1,str4.length-1) +
+str4.substring(str4.length-1,str4.length).toLowerCase();
+
+console.log(dResult);
 
 /*e. Crear una variable de tipo string con al menos 10 caracteres y algún espacio en blanco.
 Encontrar la posición del primer espacio en blanco y guardarla en una variable (utilizar indexOf).*/
+
+console.log('02.e:');
+
+var str5 = 'string cinco de al menos 10 caracteres';
+var eResult = str5.indexOf(' ');
+
+console.log(eResult);
 
 /*f. Crear una variable de tipo string con al menos 2 palabras largas (10 caracteres y algún espacio entre medio).
 Utilizar los métodos de los ejercicios anteriores para generar
 un nuevo string que tenga la primera letra de ambas palabras en mayúscula
 y las demás letras en minúscula (utilizar indexOf, substring, toUpperCase, toLowerCase y el operador +).*/
+
+console.log('02.f:');
+
+var str6 = 'dEsOxIrRiBoNuClEiCo eLeCtRoEnCeFaLoGrAfIsTa';
+var fResult =(str6.substring(0,1).toUpperCase() + str6.substring(1,str6.indexOf(' ')).toLowerCase() +
+str6.substring(str6.indexOf(' '), str6.indexOf(' ')+2).toUpperCase() +
+str6.substring(str6.indexOf(' ')+2, str6.length).toLowerCase());
+
+console.log(fResult);
